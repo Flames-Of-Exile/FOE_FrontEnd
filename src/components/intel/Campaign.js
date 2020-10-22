695307580c7242100ef533df6b70130dbf1d8db0
TimothyAL <35358436+TimothyAL@users.noreply.github.com>
2020-10-22 14:35:53 -0700

    some formating and name changes

--- src/components/intel/Campaign.js
+++ src/components/intel/Campaign.js
@@ -25,7 +25,7 @@ class Campaign extends React.Component {
             <div>
             <p className='banner'>{this.state.campaign.name}</p>
             <p>The campaign ID is {this.state.campaign.id}</p>
-            <img src={this.state.campaign.image} alt='Faled to Load Campaign'/>
+            <img src={this.state.campaign.image} className='campaign' alt='Faled to Load Campaign'/>
             {this.state.loaded ? this.state.campaign.worlds.map( (world) => (
             <World key={world.id}
                     id={world.id}
--- src/components/intel/PinDetails.js
+++ src/components/intel/PinDetails.js
@@ -3,7 +3,7 @@ import React from "react";
 function PinDetails(props) {
     return (
         <div style={{visibility: props.visibility}} className='pindetails'>
-            {props.details} <br/>
+            {props.details.name} <br/>
             Amount: {props.details.amount} Rank: {props.details.rank} Respawn: {props.details.respawn}<br/>
             Notes: {props.details.notes} id: {props.details.id}
         </div>
