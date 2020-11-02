c753107934219cae9a51105175229b031b853f61
Mark Howard <mark.c.howard.19@gmail.com>
2020-11-02 17:52:06 -0600

    fixed world requiring refresh

--- src/components/intel/CampaignSelector.js
+++ src/components/intel/CampaignSelector.js
@@ -1,120 +1,255 @@
-import React from 'react';
+import React, {
+    useEffect,
+    useState
+} from 'react';
 
 import {
     Switch,
     Route,
-  } from "react-router-dom";
+} from 'react-router-dom';
 
-import Campaign from './Campaign.js';
-import World from "./World";
-import NewWorld from "./NewWorld";
-import Socket from "../../helper_functions/Socket";
+import Campaign from './Campaign';
+import World from './World';
+import NewWorld from './NewWorld';
+import Socket from '../../helper_functions/Socket';
 
 const axios = require('axios').default;
 
-class CampaignSelector extends React.Component {
-    constructor(props) {
-        super();
-        this.state = {
-            Application: props.Application,
-            campaigns: [],
-            activeCampaign: { worlds: [], name: "" },
-            selectedIndex: -1,
-            activeWorld: { pins: []},
-        };
-        this.socket = new Socket();
-    }
+function CampaignSelector(props) {
+    const [state, setState] = useState({
+        Application: props.Application,
+        campaigns: [],
+        activeCampaign: { worlds: [], name: '' },
+        selectedIndex: -1,
+        activeWorld: { pins: [], name: '' }
+    });
 
-    async componentDidMount() {
-        this.socket.connect();
-        this.socket.registerListener('campaign-update', this.handleCampaignUpdate);
-        const response = await axios.get(`/api/campaigns`);
-        let campaigns = response.data;
-        let activeCampaign = response.data[0];
-        let activeWorld = { pins: []};
-        if (this.props.match.params.campaign) {
-            activeCampaign = campaigns.filter(campaign => campaign.name === this.props.match.params.campaign)[0];
-            if (this.props.match.params.world) {
-                activeWorld = activeCampaign.worlds.filter(world => world.name === this.props.match.params.world)[0];
+    useEffect(() => {
+        const socket = new Socket();
+        socket.connect();
+        socket.registerListener('campaign-update', handleCampaignUpdate);
+        
+        async function fetchData() {
+            const response = await axios.get(`/api/campaigns`);
+            let campaigns = response.data;
+            let activeCampaign = campaigns[0];
+            let activeWorld = { pins: [], name: '' };
+            if (props.match.params.campaign) {
+                activeCampaign = campaigns.filter(campaign => campaign.name === props.match.params.campaign)[0];
+                if (props.match.params.world) {
+                    activeWorld = activeCampaign.worlds.filter(world => world.name === props.match.params.world)[0];
+                    console.log(activeWorld)
+                }
+            }
+            setState({
+                ...state,
+                campaigns: campaigns,
+                activeCampaign: activeCampaign,
+                selectedIndex: campaigns.indexOf(activeCampaign),
+                activeWorld: activeWorld,
+            })
+            if (activeCampaign && props.location.pathname === "/campaigns") {
+                props.history.push(`/campaigns/${activeCampaign.name}`);
             }
         }
-        await this.setState({
-            ...this.state,
-            campaigns: campaigns,
-            activeCampaign: activeCampaign,
-            selectedIndex: campaigns.indexOf(activeCampaign),
-            activeWorld: activeWorld,
-        });
-        if (activeCampaign && this.props.location.pathname === "/campaigns") {
-            this.props.history.push(`/campaigns/${activeCampaign.name}`);
+
+        fetchData();
+
+        return () => {
+            socket.disconnect();
+        };
+    }, []);
+
+    useEffect(() => {
+        let activeWorld = { pins: [], name: '' };
+        if (props.match.params.world) {
+            activeWorld = state.activeCampaign.worlds.filter(world => world.name === props.match.params.world)[0];
         }
-    }
+        setState({
+            ...state,
+            activeWorld: activeWorld,
+        })
+    }, [props.match.params.world]);
 
-    componentWillUnmount() {
-        this.socket.disconnect();
-    }
-
-    handleCampaignUpdate = (data) => {
+    const handleCampaignUpdate = (data) => {
         let activeCampaign = data[0];
         let activeWorld = { pins: [] };
-        if (this.props.match.params.campaign) {
-            activeCampaign = data.filter(campaign => campaign.name === this.props.match.params.campaign)[0];
-            if (this.props.match.params.world) {
-                activeWorld = activeCampaign.worlds.filter(world => world.name === this.props.match.params.world)[0];
+        if (props.match.params.campaign) {
+            activeCampaign = data.filter(campaign => campaign.name === props.match.params.campaign)[0];
+            if (props.match.params.world) {
+                activeWorld = activeCampaign.worlds.filter(world => world.name === props.match.params.world)[0];
             }
         }
-        this.setState({
-            ...this.state,
+        setState({
+            ...state,
             campaigns: data,
             activeCampaign: activeCampaign,
             activeWorld: activeWorld,
         });
     }
 
-    handleChange = (event) => {
+    const handleChange = (event) => {
         let index = event.target.value;
         if (index == -1) {
             return;
         }
-        this.props.history.push(`/campaigns/${this.state.campaigns[index].name}`);
-        this.setState({
-            ...this.state,
-            activeCampaign: this.state.campaigns[index],
+        props.history.push(`/campaigns/${state.campaigns[index].name}`);
+        setState({
+            ...state,
+            activeCampaign: state.campaigns[index],
             selectedIndex: index
         });
     }
 
-    render() {
-        return(
-            <div>
-                <select name='activeCampaign'
-                        id='selector'
-                        placeholder='Please Choose a Campaign'
-                        onChange={this.handleChange}
-                        value={this.state.selectedIndex}
-                >
-                    <option value={-1}>Please Choose a Campaign</option>
-                    {this.state.campaigns.map( (campaign, index) => ( 
-                        <option key={index} value={index}>{campaign.name}</option>
-                    ))}
-                </select>
-                <Switch>
-                    <Route exact path="/campaigns/:campaign/addworld" render={props => <NewWorld {...props}
-                                                                                        Application={this.props.Application}
-                                                                                        campaign={this.state.activeCampaign}/>}
-                                                                                        />
-                    <Route exact path="/campaigns/:campaign/:world" render={props => <World {...props} 
-                                                                                      Application={this.props.Application}
-                                                                                      world={this.state.activeWorld} />}
-                                                                                      />
-                    <Route exact path="/campaigns/:campaign" render={props => <Campaign {...props}
-                                                                               Application={this.props.Application}
-                                                                               campaign={this.state.activeCampaign} />}
-                                                                               />
-                </Switch>
-            </div>
-        );
-    }
+    return(
+        <div>
+            <select name='activeCampaign'
+                    id='selector'
+                    placeholder='Please Choose a Campaign'
+                    onChange={handleChange}
+                    value={state.selectedIndex}
+            >
+                <option value={-1}>Please Choose a Campaign</option>
+                {state.campaigns.map( (campaign, index) => ( 
+                    <option key={index} value={index}>{campaign.name}</option>
+                ))}
+            </select>
+            <Switch>
+                <Route exact path="/campaigns/:campaign/addworld" render={props => <NewWorld {...props}
+                                                                                    Application={state.Application}
+                                                                                    campaign={state.activeCampaign}/>}
+                                                                                    />
+                <Route exact path="/campaigns/:campaign/:world" render={props => <World {...props} 
+                                                                                    Application={state.Application}
+                                                                                    world={state.activeWorld} />}
+                                                                                    />
+                <Route exact path="/campaigns/:campaign" render={props => <Campaign {...props}
+                                                                            Application={state.Application}
+                                                                            campaign={state.activeCampaign} />}
+                                                                            />
+            </Switch>
+        </div>
+    );
 }
 
 export default CampaignSelector;
+// import React from 'react';
+
+// import {
+//     Switch,
+//     Route,
+//   } from "react-router-dom";
+
+// import Campaign from './Campaign.js';
+// import World from "./World";
+// import NewWorld from "./NewWorld";
+// import Socket from "../../helper_functions/Socket";
+
+// const axios = require('axios').default;
+
+// class CampaignSelector extends React.Component {
+//     constructor(props) {
+//         super();
+//         this.state = {
+//             Application: props.Application,
+//             campaigns: [],
+//             activeCampaign: { worlds: [], name: "" },
+//             selectedIndex: -1,
+//             activeWorld: { pins: []},
+//         };
+//         this.socket = new Socket();
+//     }
+
+//     async componentDidMount() {
+//         this.socket.connect();
+//         this.socket.registerListener('campaign-update', this.handleCampaignUpdate);
+//         const response = await axios.get(`/api/campaigns`);
+//         let campaigns = response.data;
+//         let activeCampaign = response.data[0];
+//         let activeWorld = { pins: []};
+//         if (this.props.match.params.campaign) {
+//             activeCampaign = campaigns.filter(campaign => campaign.name === this.props.match.params.campaign)[0];
+//             if (this.props.match.params.world) {
+//                 activeWorld = activeCampaign.worlds.filter(world => world.name === this.props.match.params.world)[0];
+//             }
+//         }
+//         await this.setState({
+//             ...this.state,
+//             campaigns: campaigns,
+//             activeCampaign: activeCampaign,
+//             selectedIndex: campaigns.indexOf(activeCampaign),
+//             activeWorld: activeWorld,
+//         });
+//         if (activeCampaign && this.props.location.pathname === "/campaigns") {
+//             this.props.history.push(`/campaigns/${activeCampaign.name}`);
+//         }
+//     }
+
+//     componentWillUnmount() {
+//         this.socket.disconnect();
+//     }
+
+//     handleCampaignUpdate = (data) => {
+//         let activeCampaign = data[0];
+//         let activeWorld = { pins: [] };
+//         if (this.props.match.params.campaign) {
+//             activeCampaign = data.filter(campaign => campaign.name === this.props.match.params.campaign)[0];
+//             if (this.props.match.params.world) {
+//                 activeWorld = activeCampaign.worlds.filter(world => world.name === this.props.match.params.world)[0];
+//             }
+//         }
+//         this.setState({
+//             ...this.state,
+//             campaigns: data,
+//             activeCampaign: activeCampaign,
+//             activeWorld: activeWorld,
+//         });
+//     }
+
+//     handleChange = (event) => {
+//         let index = event.target.value;
+//         if (index == -1) {
+//             return;
+//         }
+//         this.props.history.push(`/campaigns/${this.state.campaigns[index].name}`);
+//         this.setState({
+//             ...this.state,
+//             activeCampaign: this.state.campaigns[index],
+//             selectedIndex: index
+//         });
+//     }
+
+//     render() {
+//         return(
+//             <div>
+//                 <select name='activeCampaign'
+//                         id='selector'
+//                         placeholder='Please Choose a Campaign'
+//                         onChange={this.handleChange}
+//                         value={this.state.selectedIndex}
+//                 >
+//                     <option value={-1}>Please Choose a Campaign</option>
+//                     {this.state.campaigns.map( (campaign, index) => ( 
+//                         <option key={index} value={index}>{campaign.name}</option>
+//                     ))}
+//                 </select>
+//                 <Switch>
+//                     <Route exact path="/campaigns/:campaign/addworld" render={props => <NewWorld {...props}
+//                                                                                         Application={this.props.Application}
+//                                                                                         campaign={this.state.activeCampaign}/>}
+//                                                                                         />
+//                     <Route exact path="/campaigns/:campaign/:world" render={props => <World {...props} 
+//                                                                                       Application={this.props.Application}
+//                                                                                       world={this.state.activeWorld} />}
+//                                                                                       />
+//                     <Route exact path="/campaigns/:campaign" render={props => <Campaign {...props}
+//                                                                                Application={this.props.Application}
+//                                                                                campaign={this.state.activeCampaign} />}
+//                                                                                />
+//                 </Switch>
+//             </div>
+//         );
+//     }
+// }
+
+// export default CampaignSelector;
--- src/components/intel/World.js
+++ src/components/intel/World.js
@@ -14,16 +14,6 @@ class World extends React.Component {
         };
     }
 
-    async componentDidMount() {
-        let campaignName = this.props.match.params.campaign;
-        let worldName = this.props.match.params.world;
-        const response = await axios.get(`/api/worlds/q?campaign=${campaignName}&world=${worldName}`);
-        this.setState({
-            ...this.state,
-            world: response.data,
-        });
-    }
-
     addNewPin = (e) => {
         var worldLoc = e.target.getBoundingClientRect();
         var leftOffsetAbs = e.clientX - worldLoc.left;
@@ -46,32 +36,36 @@ class World extends React.Component {
     }
 
     render() {
-        return (
-            <div>
-                <p className='banner'>{this.props.world.name}</p>
-                <div className='world' >
-                    <img
-                        src={this.props.world.image}
-                        onClick={this.addNewPin}
-                        alt='World Failed to load you should refresh the application'
-                    />
-                    {this.props.world.pins.map(point => (
-                        <Pin key={point.id.toString()}
-                            pin={point}
+        if (this.props.world) {
+            return (
+                <div>
+                    <p className='banner'>{this.props.world.name}</p>
+                    <div className='world' >
+                        <img
+                            src={this.props.world.image}
+                            onClick={this.addNewPin}
+                            alt='World Failed to load you should refresh the application'
                         />
-                    ))}
+                        {this.props.world.pins.map(point => (
+                            <Pin key={point.id.toString()}
+                                pin={point}
+                            />
+                        ))}
+                    </div>
+                    {this.state.newPin ? // if a new pin is being made
+                        <NewPin position_x={this.state.newPinPosition[0]} 
+                                position_y={this.state.newPinPosition[1]}
+                                world_id={this.props.world.id}
+                                onSubmit={this.cancelPin}
+                                onCancel={this.cancelPin}
+                        />
+                    : null// else
+                    /*end if new pin is being made*/}
                 </div>
-                {this.state.newPin ? // if a new pin is being made
-                    <NewPin position_x={this.state.newPinPosition[0]} 
-                            position_y={this.state.newPinPosition[1]}
-                            world_id={this.state.world.id}
-                            onSubmit={this.cancelPin}
-                            onCancel={this.cancelPin}
-                    />
-                : null// else
-                /*end if new pin is being made*/}
-            </div>
-        );
+            );
+        } else {
+            return (<div></div>)
+        }
     }
 }
 
