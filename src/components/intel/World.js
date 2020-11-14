c7617a3f891187942f05fb46a541c5f882b93b27
Mark Howard <mark.c.howard.19@gmail.com>
2020-11-14 15:03:01 -0600

    wip, map loads and can add pins

--- src/components/intel/World.js
+++ src/components/intel/World.js
@@ -1,89 +1,115 @@
 import React from "react";
+import { MapContainer, ImageOverlay, Marker, Popup } from 'react-leaflet';
 import Pin from './Pin.js';
 import PinDetails from './PinDetails.js';
 import NewPin from './NewPin.js';
+import { CRS, Icon } from "leaflet";
 
-class World extends React.Component {
-    constructor(props) {
-        super();
-        this.cancelPin = this.cancelPin.bind(this);
-        this.state = {
-            newPin: false,
-            newPinPosition: [0,0],
-            Application: props.Application
-        };
-    }
+const well = new Icon({
+    iconUrl: "/staticfiles/icons/well.png",
+    iconSize: [25, 50],
+    iconAnchor: [12.5, 50]
+});
 
-    addNewPin = (e) => {
-        var worldLoc = e.target.getBoundingClientRect();
-        var leftOffsetAbs = e.clientX - worldLoc.left;
-        var bottomOffsetAbs = worldLoc.bottom - e.clientY;
-        const leftPercent = (leftOffsetAbs)/(worldLoc.right - worldLoc.left)*100-1.3;
-        const bottomPercent = (bottomOffsetAbs)/(worldLoc.bottom - worldLoc.top)*100;
-        const placePin = [leftPercent, bottomPercent];
-        this.setState({
-            ...this.state,
-            newPin: true,
-            newPinPosition: placePin
-        });
-    }
-
-    cancelPin() {
-        this.setState({
-            ...this.state,
-            newPin:false
-        });
-    }
-
-    componentDidUpdate() {
-        if (this.props.Application.state !== this.state.Application.state) {
-            this.setState({
-                ...this.state,
-                Application: this.props.Application
-            });
-        }
-    }
-
-    render() {
-        if (this.props.world) {
-            return (
-                <div>
-                    <p className='banner'>{this.props.world.name}</p>
-                    <div className='world' >
-                        <img
-                            src={this.props.world.image}
-                            onClick={this.addNewPin}
-                            alt='World Failed to load you should refresh the application'
-                        />
-                        {this.props.world.pins.map(point => (
-                            <>
-                                <Pin key={point.id.toString()}
-                                    pin={point}
-                                    Application={this.props.Application}
-                                />
-                                <PinDetails key={point.id.toString() + 'details'}
-                                    details={point}
-                                    Application={this.props.Application}
-                                />
-                            </>
-                        ))}
-                    </div>
-                    {this.state.newPin ? // if a new pin is being made
-                        <NewPin position_x={this.state.newPinPosition[0]} 
-                                position_y={this.state.newPinPosition[1]}
-                                world_id={this.props.world.id}
-                                onSubmit={this.cancelPin}
-                                onCancel={this.cancelPin}
-                        />
-                    : null// else
-                    /*end if new pin is being made*/}
-
-                </div>
-            );
-        } else {
-            return (<div></div>);
-        }
-    }
+function World(props) {
+    const position = [0, 0];
+    return (
+        <div>
+            <MapContainer center={position} zoom={-1} scrollWheelZoom={true} crs={CRS.Simple} minZoom={-3} >
+                <NewPin world_id={props.world.id}/>
+                <ImageOverlay url={props.world.image} bounds={[[-1000,-2000], [1000,2000]]} />
+                {props.world.pins.map(pin => <Marker
+                                                key={pin}
+                                                icon={well}
+                                                position={[pin.position_y, pin.position_x]}>
+                                                <Popup offset={[0, -10]}>{pin.name}</Popup>
+                                            </Marker>)}
+            </MapContainer>
+        </div>
+    );
 }
+
+// class World extends React.Component {
+//     constructor(props) {
+//         super();
+//         this.cancelPin = this.cancelPin.bind(this);
+//         this.state = {
+//             newPin: false,
+//             newPinPosition: [0,0],
+//             Application: props.Application
+//         };
+//     }
+
+//     addNewPin = (e) => {
+//         var worldLoc = e.target.getBoundingClientRect();
+//         var leftOffsetAbs = e.clientX - worldLoc.left;
+//         var bottomOffsetAbs = worldLoc.bottom - e.clientY;
+//         const leftPercent = (leftOffsetAbs)/(worldLoc.right - worldLoc.left)*100-1.3;
+//         const bottomPercent = (bottomOffsetAbs)/(worldLoc.bottom - worldLoc.top)*100;
+//         const placePin = [leftPercent, bottomPercent];
+//         this.setState({
+//             ...this.state,
+//             newPin: true,
+//             newPinPosition: placePin
+//         });
+//     }
+
+//     cancelPin() {
+//         this.setState({
+//             ...this.state,
+//             newPin:false
+//         });
+//     }
+
+//     componentDidUpdate() {
+//         if (this.props.Application.state !== this.state.Application.state) {
+//             this.setState({
+//                 ...this.state,
+//                 Application: this.props.Application
+//             });
+//         }
+//     }
+
+//     render() {
+//         if (this.props.world) {
+//             return (
+//                 <div>
+//                     <p className='banner'>{this.props.world.name}</p>
+//                     <div className='world' >
+//                         <img
+//                             src={this.props.world.image}
+//                             onClick={this.addNewPin}
+//                             alt='World Failed to load you should refresh the application'
+//                         />
+//                         {this.props.world.pins.map(point => (
+//                             <>
+//                                 <Pin key={point.id.toString()}
+//                                     pin={point}
+//                                     Application={this.props.Application}
+//                                 />
+//                                 <PinDetails key={point.id.toString() + 'details'}
+//                                     details={point}
+//                                     Application={this.props.Application}
+//                                 />
+//                             </>
+//                         ))}
+//                     </div>
+//                     {this.state.newPin ? // if a new pin is being made
+//                         <NewPin position_x={this.state.newPinPosition[0]} 
+//                                 position_y={this.state.newPinPosition[1]}
+//                                 world_id={this.props.world.id}
+//                                 onSubmit={this.cancelPin}
+//                                 onCancel={this.cancelPin}
+//                         />
+//                     : null// else
+//                     /*end if new pin is being made*/}
+
+//                 </div>
+//             );
+//         } else {
+//             return (<div></div>);
+//         }
+//     }
+// }
 
 export default World;
